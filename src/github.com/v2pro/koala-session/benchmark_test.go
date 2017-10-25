b577994c50c3d9be974aede8bba04bd72b288b4c
Tao Wen <taowen@gmail.com>
2017-10-25 17:12:02 +0800

    encode actions

--- src/github.com/v2pro/koala-session/benchmark_test.go
+++ src/github.com/v2pro/koala-session/benchmark_test.go
@@ -6,6 +6,7 @@ import (
 	"bytes"
 	"encoding/hex"
 	"github.com/v2pro/koala/recording"
+	"net"
 )
 
 func Benchmark_original_decoder(b *testing.B) {
@@ -63,6 +64,29 @@ func Benchmark_faster_decoder(b *testing.B) {
 	}
 }
 
+func Benchmark_original_encoder(b *testing.B) {
+	input, err := hex.DecodeString("0b00010000000a73657373696f6e2d69640c00020c00010a000100000000000000010a000200000000000000000b00030000000f43616c6c46726f6d496e626f756e64000c00020b0001000000093132372e302e302e310a000200000000000004d2000b00030000000568656c6c6f000c00030c00010a000100000000000000020a000200000000000000000b00030000000d52657475726e496e626f756e64000b000200000005776f726c64000f00040c000000010c00020c00010a000100000000000000020a000200000000000000000b00030000000d52657475726e496e626f756e64000b000200000005776f726c64000000")
+	if err != nil {
+		b.Error(err)
+		return
+	}
+	buf := &memoryBuffer{}
+	proto := thrift.NewTBinaryProtocol(buf, true, true)
+	session1 := &Session{}
+	buf.Buffer = bytes.NewBuffer(input)
+	err = session1.Read(proto)
+	if err != nil {
+		b.Error(err)
+		return
+	}
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		buf.Buffer.Reset()
+		session1.Write(proto)
+	}
+}
+
 func Benchmark_fast_encoder(b *testing.B) {
 	input, err := hex.DecodeString("0b00010000000a73657373696f6e2d69640c00020c00010a000100000000000000010a000200000000000000000b00030000000f43616c6c46726f6d496e626f756e64000c00020b0001000000093132372e302e302e310a000200000000000004d2000b00030000000568656c6c6f000c00030c00010a000100000000000000020a000200000000000000000b00030000000d52657475726e496e626f756e64000b000200000005776f726c64000f00040c000000010c00020c00010a000100000000000000020a000200000000000000000b00030000000d52657475726e496e626f756e64000b000200000005776f726c64000000")
 	if err != nil {
@@ -83,7 +107,26 @@ func Benchmark_fast_encoder(b *testing.B) {
 	session2.CallFromInbound = &recording.CallFromInbound{
 		BaseAction: recording.BaseAction{
 			ActionIndex: int(session1.CallFromInbound.Base.ActionIndex),
+			OccurredAt:  session1.CallFromInbound.Base.OccurredAt,
+			ActionType:  session1.CallFromInbound.Base.ActionType,
 		},
+		Peer: net.TCPAddr{
+			IP:   net.ParseIP(session1.CallFromInbound.Peer.IP),
+			Port: int(session1.CallFromInbound.Peer.Port),
+			Zone: session1.CallFromInbound.Peer.Zone,
+		},
+		Request: session1.CallFromInbound.Request,
+	}
+	session2.ReturnInbound = &recording.ReturnInbound{
+		BaseAction: recording.BaseAction{
+			ActionIndex: int(session1.ReturnInbound.Base.ActionIndex),
+			OccurredAt:  session1.ReturnInbound.Base.OccurredAt,
+			ActionType:  session1.ReturnInbound.Base.ActionType,
+		},
+		Response: session1.ReturnInbound.Response,
+	}
+	session2.Actions = []recording.Action{
+		session2.ReturnInbound,
 	}
 	encoder := &SessionEncoder{}
 	b.ResetTimer()
--- src/github.com/v2pro/koala-session/encode_session.go
+++ src/github.com/v2pro/koala-session/encode_session.go
@@ -3,6 +3,7 @@ package koala_session
 import (
 	"github.com/v2pro/koala/recording"
 	"unsafe"
+	"net"
 )
 
 type SessionEncoder struct {
@@ -19,17 +20,68 @@ func (encoder *SessionEncoder) Encode(session *recording.Session) {
 		encoder.Output = append(encoder.Output, []byte{0x0c, 0x00, 0x02}...)
 		encoder.encodeCallFromInbound(session.CallFromInbound)
 	}
+	if session.ReturnInbound != nil {
+		encoder.Output = append(encoder.Output, []byte{0x0c, 0x00, 0x03}...)
+		encoder.encodeReturnInbound(session.ReturnInbound)
+	}
+	if len(session.Actions) > 0 {
+		encoder.Output = append(encoder.Output, []byte{0x0f, 0x00, 0x04}...)
+		encoder.encodeUInt32(uint32(len(session.Actions)))
+		for _, action := range session.Actions {
+			encoder.encodeAction(action)
+		}
+	}
+	encoder.Output = append(encoder.Output, 0x00)
+}
+
+func (encoder *SessionEncoder) encodeAction(action recording.Action) {
+	switch typedAction := action.(type) {
+	case *recording.ReturnInbound:
+		encoder.encodeReturnInbound(typedAction)
+	}
+}
+
+func (encoder *SessionEncoder) encodeReturnInbound(returnInbound *recording.ReturnInbound) {
+	encoder.Output = append(encoder.Output, []byte{0x0c, 0x00, 0x01}...)
+	encoder.encodeBaseAction(&returnInbound.BaseAction)
+	if len(returnInbound.Response) > 0 {
+		encoder.Output = append(encoder.Output, []byte{0x0b, 0x00, 0x02}...)
+		encoder.encodeBinary(returnInbound.Response)
+	}
+	encoder.Output = append(encoder.Output, 0x00)
 }
 
 func (encoder *SessionEncoder) encodeCallFromInbound(callFromInbound *recording.CallFromInbound) {
 	encoder.Output = append(encoder.Output, []byte{0x0c, 0x00, 0x01}...)
 	encoder.encodeBaseAction(&callFromInbound.BaseAction)
+	encoder.Output = append(encoder.Output, []byte{0x0c, 0x00, 0x02}...)
+	encoder.encodePeer(&callFromInbound.Peer)
+	if len(callFromInbound.Request) > 0 {
+		encoder.Output = append(encoder.Output, []byte{0x0b, 0x00, 0x03}...)
+		encoder.encodeBinary(callFromInbound.Request)
+	}
 	encoder.Output = append(encoder.Output, 0x00)
 }
 
 func (encoder *SessionEncoder) encodeBaseAction(baseAction *recording.BaseAction) {
 	encoder.Output = append(encoder.Output, []byte{0x0a, 0x00, 0x01}...)
 	encoder.encodeUInt64(uint64(baseAction.ActionIndex))
+	encoder.Output = append(encoder.Output, []byte{0x0a, 0x00, 0x02}...)
+	encoder.encodeUInt64(uint64(baseAction.OccurredAt))
+	encoder.Output = append(encoder.Output, []byte{0x0b, 0x00, 0x03}...)
+	encoder.encodeString(baseAction.ActionType)
+	encoder.Output = append(encoder.Output, 0x00)
+}
+
+func (encoder *SessionEncoder) encodePeer(peer *net.TCPAddr) {
+	encoder.Output = append(encoder.Output, []byte{0x0b, 0x00, 0x01}...)
+	encoder.encodeBinary(peer.IP)
+	encoder.Output = append(encoder.Output, []byte{0x0a, 0x00, 0x02}...)
+	encoder.encodeUInt64(uint64(peer.Port))
+	if len(peer.Zone) > 0 {
+		encoder.Output = append(encoder.Output, []byte{0x0b, 0x00, 0x03}...)
+		encoder.encodeString(peer.Zone)
+	}
 	encoder.Output = append(encoder.Output, 0x00)
 }
 
@@ -39,6 +91,16 @@ func (encoder *SessionEncoder) encodeUInt64(val uint64) {
 		byte(val >> 48),
 		byte(val >> 40),
 		byte(val >> 32),
+		byte(val >> 24),
+		byte(val >> 16),
+		byte(val >> 8),
+		byte(val),
+	}
+	encoder.Output = append(encoder.Output, tmp...)
+}
+
+func (encoder *SessionEncoder) encodeUInt32(val uint32) {
+	tmp := []byte{
 		byte(val >> 24),
 		byte(val >> 16),
 		byte(val >> 8),
